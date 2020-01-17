c88111b4394b5d3fdd09d875d4744ea1a9cb6f9a
sh <sommerh143@yahoo.com>
2020-01-17 10:24:07 -0800

    all the buttons work and able to move on to next question

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,24 @@ $(document).ready(function() {
     $("#question1").show();
   });
   $("#q1").click(function(){
-    var ageanswer =$("input:radio[name=age]:checked").val();
-
+    var ageanswer1 =$("input:radio[name=age]:checked").val();
+    $("#question2").show();
   });
+  $("#q2").click(function(){
+    var ageanswer2 =$("input:radio[name=music]:checked").val();
+    $("#question3").show();
+  });
+  $("#q3").click(function(){
+    var ageanswer3 =$("input:radio[name=drink]:checked").val();
+    $("#question4").show();
+  });
+  $("#q4").click(function(){
+    var foodanswer = $("#food").val();
+    $("#question5").show();
+  });
+  $("#q5").click(function(){
+    var vacationanswer = $("#vacation").val();
+    alert(ageanswer1);
+  });
+  
 });
