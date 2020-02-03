17d58d3e3ba7649b06fccecc218d7b175ea0a6de
Student <example@example.com>
2020-02-03 12:51:29 -0800

    fix my variables and make the final answer show  after answering all the questions

--- js/scripts.js
+++ js/scripts.js
@@ -2,15 +2,16 @@ $(document).ready(function() {
   $("#start").click(function(){
     $("#question1").show();
   });
+  var answer1, answer2,answer3, answer4, answer5 = "";
   $("#q1").click(function(){
-    var answer1 = $("input:radio[name=age]:checked").val();
-    if (answer1 === "0-11") {
+    answer1 = $("input:radio[name=age]:checked").val();
+    if (answer1 === "1") {
       $("#babypic").show();
       $("#question2").show();
-    } else if (answer1 === "12-19") {
+    } else if (answer1 === "2") {
       $("#teenpic").show();
       $("#question2").show();
-    } else if (answer1 === "20-30") {
+    } else if (answer1 === "3") {
       $("#adultpic").show();
       $("#question2").show();
     } else {
@@ -19,67 +20,67 @@ $(document).ready(function() {
     }    
   });
   $("#q2").click(function(){
-    var answer2 = $("input:radio[name=music]:checked").val();
+    answer2 = $("input:radio[name=music]:checked").val();
     $("#question3").show();
    
-   if (answer2 === "rock"){
+   if (answer2 === "1"){
      $("#rockpic").show();
-   } else if (answer2 === "rap"){
+   } else if (answer2 === "2"){
     $("#rappic").show();
-   } else if (answer2 === "country"){ 
+   } else if (answer2 === "3"){ 
     $("#countrypic").show();
    } else {
     $("#edmpic").show();
    }
   });
   $("#q3").click(function(){
-    var answer3 = $("input:radio[name=drink]:checked").val();
+    answer3 = $("input:radio[name=drink]:checked").val();
     $("#question4").show();
-    if (answer3 === "coffee"){
+    if (answer3 === "1"){
      $("#cofeepic").show();
-    } else if (answer3 === "tea"){ 
+    } else if (answer3 === "2"){ 
      $("#teapic").show();
-    } else if (answer3 === "smoothie") {
+    } else if (answer3 === "3") {
      $("#smoothiepic").show();
     } else {
       $("#waterpic").show();
     }
   });
   $("#q4").click(function(){
-    var answer4 = $("input:radio[name=food]:checked").val();
+    answer4 = $("input:radio[name=food]:checked").val();
     $("#question5").show();
-    if (answer4 === "mexican"){
+    if (answer4 === "1"){
       $("#mexicanpic").show();
-     } else if (answer4 === "veggi"){ 
+     } else if (answer4 === "3"){ 
       $("#veggipic").show();
-     } else if (answer4 === "italian") {
+     } else if (answer4 === "2") {
       $("#italianpic").show();
      } else {
        $("#burgerpic").show();
      }
   });
   $("#q5").click(function(){
-    var answer5 = $("input:radio[name=vacation]:checked").val();
-    if (answer5=== "beach"){
+    answer5 = $("input:radio[name=vacation]:checked").val();
+    if (answer5=== "1"){
       $("#beachpic").show();
-     } else if (answer5 === "tropical"){ 
+     } else if (answer5 === "2"){ 
       $("#tropicalpic").show();
-     } else if (answer5 === "artic") {
+     } else if (answer5 === "3") {
       $("#articpic").show();
      }
-    if (answer1 === "0-11" && answer2 === "rock" && answer3 === "coffee" && answer4 === "mexican" && answer5 === "beach") {
+    if (answer1 === "1" && answer2 === "1" && answer3 === "1" && answer4 === "1" && answer5 === "1") {
       $("#csharp").show();
       $("#java").hide();
       $("#cplus").hide();
       $("#ruby").hide();
       $("#info2").show();
-    } else if (answer1 === "12-19" && answer2 === "rap" && answer3 === "tea" && answer4 === "burgers" && answer5 === "tropical") {
+    } else if (answer1 === "2" && answer2 === "2" && answer3 === "2" && answer4 === "2" && answer5 === "2") {
       $("#java").show();
       $("#csharp").hide();
       $("#cplus").hide();
       $("#ruby").hide();
       $("#info3").show();
-    } else if (answer1 === "20-30" && answer2 === "country" && answer3 === "smoothie" && answer4 === "italian" && answer5 === "artic") {
+    } else if (answer1 === "3" && answer2 === "3" && answer3 === "3" && answer4 === "3" && answer5 === "3") {
       $("#java").hide();
       $("#csharp").hide();
       $("#cplus").show();
